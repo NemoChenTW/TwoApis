e8e7752ea92396b2bec081df1d77e1816400ede8
NemoChen <nemochentw@gmail.com>
2020-06-22 17:18:37 +0800

    Add API query chain

--- app/src/main/java/com/nemochen/twoapis/model/StatusRepository.kt
+++ app/src/main/java/com/nemochen/twoapis/model/StatusRepository.kt
@@ -1,22 +1,45 @@
 package com.nemochen.twoapis.model
 
 import kotlinx.coroutines.delay
+import retrofit2.HttpException
+import java.lang.Exception
+import java.net.SocketTimeoutException
 
 class StatusRepository {
     companion object {
-        const val ERROR_STRING = "Error"
+        const val NETWORK_ERROR_STRING = "Network Error"
+        const val UNKNOWN_ERROR_STRING = "Unknown Error"
     }
 
-    private val statusService = RetrofitManager.getPublicAPI()
+    private val publicStatusService = RetrofitManager.getPublicAPI()
+    private val privateStatusService = RetrofitManager.getPrivateAPI()
 
-    suspend fun getStatus(): StatusData {
+    suspend fun getPublicServiceStatus(): StatusData {
+        return getStatus(publicStatusService)
+    }
+    suspend fun getPrivateServiceStatus(): StatusData {
+        return getStatus(privateStatusService)
+    }
+
+
+    private suspend fun getStatus(stateService: StatusService): StatusData {
         delay(2000)
 
-        return if (statusService.getStatus().isSuccessful) {
-            statusService.getStatus().body()!!
-        } else {
-            // TODO: The error can handle by adding CallAdapterFactory to Retrofit
-            StatusData(ERROR_STRING)
+        try {
+            return if (stateService.getStatus().isSuccessful) {
+                stateService.getStatus().body()!!
+            } else {
+                // TODO: The error can handle by adding CallAdapterFactory to Retrofit
+                StatusData(NETWORK_ERROR_STRING)
+            }
+        } catch (ex: Exception) {
+            return when (ex) {
+                is HttpException,
+                is SocketTimeoutException -> {
+                    StatusData(NETWORK_ERROR_STRING)
+                }
+                else -> StatusData(UNKNOWN_ERROR_STRING)
+            }
         }
     }
 }
--- app/src/main/java/com/nemochen/twoapis/ui/main/MainViewModel.kt
+++ app/src/main/java/com/nemochen/twoapis/ui/main/MainViewModel.kt
@@ -1,26 +1,34 @@
 package com.nemochen.twoapis.ui.main
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.nemochen.twoapis.model.StatusRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainViewModel(private val statusRepository: StatusRepository) : ViewModel() {
     companion object {
         const val LOADING_MESSAGE = "Loading"
         const val FAIL_MESSAGE = "Get status fail"
-        const val STATUS_PREFIX = "Service status: "
+        const val STATUS_PREFIX_PUBLIC = "Public service: "
+        const val STATUS_PREFIX_PRIVATE = "Private service: "
     }
 
-    val statusString = MutableLiveData<String>()
+    val publicStatusString = MutableLiveData<String>()
+    val privateStatusString = MutableLiveData<String>()
 
     fun getStatus() {
         viewModelScope.launch {
-            statusString.value = LOADING_MESSAGE
+            publicStatusString.value = STATUS_PREFIX_PUBLIC + LOADING_MESSAGE
+            privateStatusString.value = STATUS_PREFIX_PRIVATE + LOADING_MESSAGE
             launch(Dispatchers.IO) {
-                statusString.postValue(STATUS_PREFIX + statusRepository.getStatus().status)
+                runBlocking {
+                    publicStatusString.postValue(STATUS_PREFIX_PUBLIC + statusRepository.getPublicServiceStatus().status)
+                }
+
+                runBlocking {
+                    privateStatusString.postValue(STATUS_PREFIX_PRIVATE + statusRepository.getPrivateServiceStatus().status)
+                }
             }
         }
     }
