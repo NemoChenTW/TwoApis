c9254252a0b71894259f03cdd58ea78cd0ce8bc4
NemoChen <nemochentw@gmail.com>
2020-06-23 00:54:00 +0800

    Add network change between two api request

--- app/src/main/java/com/nemochen/twoapis/ui/main/MainViewModel.kt
+++ app/src/main/java/com/nemochen/twoapis/ui/main/MainViewModel.kt
@@ -1,6 +1,11 @@
 package com.nemochen.twoapis.ui.main
 
 import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
@@ -9,6 +14,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.lang.ref.WeakReference
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class MainViewModel(application: Application, private val statusRepository: StatusRepository) : AndroidViewModel(application) {
     companion object {
@@ -21,21 +28,43 @@ class MainViewModel(application: Application, private val statusRepository: Stat
     val publicStatusString = MutableLiveData<String>()
     val privateStatusString = MutableLiveData<String>()
 
-    val weakContext = WeakReference(application.applicationContext)
+    private val weakContext = WeakReference(application.applicationContext)
 
     fun getStatus() {
         viewModelScope.launch {
             publicStatusString.value = STATUS_PREFIX_PUBLIC + LOADING_MESSAGE
             privateStatusString.value = STATUS_PREFIX_PRIVATE + LOADING_MESSAGE
             launch(Dispatchers.IO) {
-                runBlocking {
-                    publicStatusString.postValue(STATUS_PREFIX_PUBLIC + statusRepository.getPublicServiceStatus().status)
-                }
 
                 runBlocking {
+                    publicStatusString.postValue(STATUS_PREFIX_PUBLIC + statusRepository.getPublicServiceStatus().status)
+                    changeNetWorkToWifi()
                     privateStatusString.postValue(STATUS_PREFIX_PRIVATE + statusRepository.getPrivateServiceStatus().status)
                 }
             }
         }
     }
+
+    private suspend fun changeNetWorkToWifi() {
+        weakContext.get()?.let {
+            val connectivityManager = it.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val request = NetworkRequest.Builder().apply {
+                addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+            }
+
+            requestNetWork(connectivityManager, request)?.let { network ->
+                connectivityManager.bindProcessToNetwork(network)
+            }
+        }
+    }
+
+    private suspend fun requestNetWork(connectivityManager: ConnectivityManager, request: NetworkRequest.Builder): Network =
+        suspendCoroutine {
+            connectivityManager.requestNetwork(request.build(),
+                object: ConnectivityManager.NetworkCallback() {
+                    override fun onAvailable(network: Network) {
+                        it.resume(network)
+                    }
+                })
+        }
 }
